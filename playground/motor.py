5f7bf622518389218efeb841a01ce8bdca42956d
Thomas Bertok <thomasbertok@gmail.com>
2020-11-11 21:47:42 +0200

    first files

---
+++ playground/motor.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+# test program for the brushed motor driven by the L298N driver
---
+++ playground/servo.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+# test program for the servo motor driven directly from the Pi
