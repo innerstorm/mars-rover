becd298d312f1991149eea1b7489dad8069190db
Thomas Bertok <thomasbertok@gmail.com>
2020-11-12 21:01:11 +0200

    update client/server

--- playground/sockets/client.py
+++ playground/sockets/client.py
@@ -1,2 +1,23 @@
-#!/usr/bin/python3
-# client
+import socket
+import sys
+import time
+
+x = socket.socket()
+host_name = input(str('Server hostname:'))
+#host_name = socket.gethostname()
+port = 8080
+
+x.connect((host_name, port))
+print('Connected to server')
+
+while True: 
+    incoming_message = x.recv(1024)
+    incoming_messagge = incoming_message.decode()
+    
+    print("Server incoming message: ", incoming_message)
+    
+    message = input(" >> ")
+    message = message.encode()
+    
+    x.send(message)
+    print("message has been sent...")
--- playground/sockets/server.py
+++ playground/sockets/server.py
@@ -1,2 +1,28 @@
-#!/usr/bin/python3
-# server
+import socket
+import sys 
+import time
+
+x = socket.socket()
+host_name = socket.gethostname()
+print('server will start on host: ', host_name)
+port = 8080
+
+x.bind((host_name, port))
+
+print("server done binding to host and port successfully")
+print("server is waiting for incoming connections...")
+
+x.listen(1)
+
+connection, address = x.accept()
+
+print(address, " has connected to the server and is now online...")
+
+while True:
+    display_mess= input(str(" >> "))
+    display_mess=display_mess.encode()
+    connection.send(display_mess)
+    print("message has been sent...")
+    in_message=connection.recv(1024)
+    in_messagge=in_message.decode()
+    print("Client says: ", in_message)
